6a6c04c99df0483a5bab0e6ccc97df4880cda679
jason871012 <871012jason@gmial.com>
2020-07-25 00:30:56 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,12 @@
 import discord
 from discord.ext import commands
+import json
 
-bot=commands.Bot(command_prefix='[')
+with open('setting.json','r',encoding='utf8')as settingfile:
+    settingdata=json.load(settingfile)
+      
+
+bot=commands.Bot(command_prefix='!')
 
 @bot.event
 async def on_ready():
@@ -10,17 +15,17 @@ async def on_ready():
 @bot.event
 async def on_member_join(member):
     print(f'{member} join!')
-    channel=bot.get_channel(736245212724330587) 
+    channel=bot.get_channel(int(settingdata['welcome'])) 
     await channel.send(f'{member}join!')
 
 @bot.event
 async def on_member_remove(member):
     print(f'{member} leave!')
-    channel=bot.get_channel(736245296610279454) 
+    channel=bot.get_channel(int(settingdata['離開'])) 
     await channel.send(f'{member}leave!')
     
 @bot.command()
 async def ping(ctx):
-    await ctx.send(bot.latency)
-    
-bot.run('NzM2MjMzOTU1MDA2NzQyNjYx.Xxr1TQ.PMA6_Zbh20uQVOu288Rs-7LwCzo')
+    await ctx.send(f'{round((bot.latency*1000),2)}(ms)')
+
+bot.run(settingdata['token'])
